d67ed59b0152a6dc5e7fbc90f1f223420f4cf4db
l.orsini <l.orsini@mazarinedigital.com>
2021-01-27 15:01:54 +0100

    update roue du meme

--- src/views/RoueDuMeme.vue
+++ src/views/RoueDuMeme.vue
@@ -3,8 +3,7 @@
     <!-- ROUE DU MEME AU CLICK -->
 
     <div class="rdm-click">
-      <img  ref="poring" class="poring" src="assets/rdm/img/poring.png" :randomHandler="alreadyStoredNumber" @click="getRandomMeme" />
-    
+      <img  ref="poring" class="poring" src="assets/rdm/img/poring.png" :randomHandler="alreadyStoredNumber" @click="getRandomMeme"/>
     </div>
 
     <!-- ROUE DU MEME SELECT OPTION -->
@@ -16,7 +15,7 @@
             </select>
         </div> -->
 
-    <br />
+    <br>
     <div v-if="randomMeme">
       <div class="image-container">
         <img :src="randomMeme.image" alt="" />
@@ -41,64 +40,93 @@
 </template>
 
 <script>
-import { TimelineLite } from "gsap";
 
-import { memes } from "@/rdmData.js";
-export default {
-  name: "RoueDuMeme",
-  mounted: () => {
-    console.log("rgergerg");
-  },
+  import { TimelineLite } from "gsap";
 
-  data() {
-    return {
-      memes: memes,
-      actualNumber: 0,
-      alreadyStoredNumber: [],
-      monAudio: "",
-    };
-  },
+  import { memes } from "@/rdmData.js";
 
-  computed: {
-    randomMeme: {
-      get: function() {
-        return this.getRandomMeme() || {};
-      },
-      set: function() {
-        return {
-          id: "100",
-          image: "static/img/memes/relance.jpg",
-          audio: "static/audio/memes/turrset.wav",
-          isDejaPasse: false,
-        };
-      },
+  export default {
+    
+    name: "RoueDuMeme",
+
+    data() {
+      return {
+        memes: memes,
+        actualNumber: 0,
+        alreadyStoredNumber: [],
+        monAudio: ""
+      };
     },
-  },
 
-  methods: {
-    getRandomMeme() {
-      this.gsap = new TimelineLite();
-      this.poring = document.getElementsByClassName("poring");
-      console.log(this.$refs.poring);
-      this.gsap.to(this.$refs.poring, {
-        duration: 2.5,
-        ease: "bounce.out",
-        y: -500,
-      });
-      let randomNumber = Math.floor(Math.random() * memes.length);
-      this.actualNumber = randomNumber;
+    computed: {
 
-      this.alreadyStoredNumber.push(randomNumber);
-      return memes[randomNumber];
+      randomMeme: {
+
+        get: function() {
+          return this.getRandomMeme;
+        },
+
+        set: function() {
+          return {
+            id: "100",
+            image: "static/img/memes/relance.jpg",
+            audio: "static/audio/memes/turrset.wav",
+            isDejaPasse: false,
+          };
+        },
+
+      },
+
     },
-  },
-};
+
+    mounted(){
+      console.log("bien mounted");
+
+      this.timeline = new TimelineLite();
+
+      console.log("ma tl : ", this.timeline);
+    },
+
+    methods: {
+
+      getRandomMeme(){
+
+        // console.log(this.$refs.poring);
+
+        let randomNumber = Math.floor(Math.random() * memes.length);
+
+        this.actualNumber = randomNumber;
+
+        this.alreadyStoredNumber.push(randomNumber);
+
+        this.animateAsset();
+
+        return memes[randomNumber];
+
+      },
+
+      animateAsset(){
+
+        console.log("anim triggered");
+
+        this.timeline.to(this.$refs.poring, 2.5, 
+          {
+            ease: "bounce.out",
+            y: -50,
+          }
+        );
+
+      }
+
+    }
+
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-img {
-  margin: 0 auto;
-  width: 200px;
-}
+<style lang="scss" scoped>
+  img {
+    margin: 0 auto;
+    width: 200px;
+  }
 </style>
