6c4b643c83aed0bfd2963eabc8627bcadc3c13af
Miel2000 <fabien.orsn@gmail.com>
2021-02-08 16:50:33 +0100

    clean playscene

--- src/components/GameCenterComponents/scenes/PlayScene.js
+++ src/components/GameCenterComponents/scenes/PlayScene.js
@@ -3,41 +3,36 @@ import { Phaser } from 'phaser';
 
 
 export default class PlayScene extends Scene {
-  
+    
     constructor () {
-
-        
         super({ key: 'PlayScene' })
     }
 
     create () {
 
- 
-// __ Background
+    // __ Background
         this.add.image(400,300, 'sky');
 
-// __ Butterflys
-   
+    // __ Butterflys
+        this.butterfly = this.physics.add.group({
+            key: 'butterfly',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
 
- this.butterfly = this.physics.add.group({
-    key: 'butterfly',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 }
-});
+        this.butterfly.children.iterate(function (child) {
 
- this.butterfly.children.iterate(function (child) {
+            child.setBounceY(0.45);
 
-    child.setBounceY(0.45);
+        });
 
-});
-// __ Physics items
+    // __ Physics items
 
-this.platforms = this.physics.add.staticGroup();
-this.platforms.create(400, 568, 'ground').refreshBody();
-this.platforms.create(400, 420, 'ground').setScale(0.5).refreshBody();
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').refreshBody();
+        this.platforms.create(400, 420, 'ground').setScale(0.5).refreshBody();
 
-
-// __ Player
+    // __ Player
         this.player =  this.physics.add.sprite(400,200, 'chevre');
         this.player.setBounce(0.4);
         this.player.setCollideWorldBounds(true);
@@ -68,27 +63,23 @@ this.platforms.create(400, 420, 'ground').setScale(0.5).refreshBody();
                 frameRate: 10,
                 repeat: -1,
             });
-        // chevre.body.onWorldBoundds = true;
-        // chevre.setVelocity(200, 20);
 
-// __ Colliders
+    // __ Colliders
         this.physics.add.collider( this.player, this.platforms);
 
-
-// __ Sound when physics
+    // __ Sound when physics
         this.sound.add('bamboo')
         this.physics.world.on('worldbounds', () => {
-             this.sound.play('bamboo', { volume: 0.75 })
+                this.sound.play('bamboo', { volume: 0.75 })
         })
 
-// Butterflys collect
+    // Butterflys collect
         this.physics.add.collider(this.butterfly, this.platforms);
         this.physics.add.overlap(this.player, this.butterfly, collectButterfly, null, this);
         function collectButterfly (player, star)
         {
             star.disableBody(true, true);
         }
-
     }
 
 
@@ -101,29 +92,23 @@ this.platforms.create(400, 420, 'ground').setScale(0.5).refreshBody();
         {
             console.log('left');
             this.player.setVelocityX(-160);
-
-           this.player.anims.play('left', true);
+            this.player.anims.play('left', true);
         }
         else if (cursors.right.isDown)
         {
             console.log('right');
-            
             this.player.setVelocityX(160);
-            
             this.player.anims.play('right', true);
         }
         else
         {
             console.log('turn');
             this.player.setVelocityX(0);
-            
             this.player.anims.play('turn');
         }
 
         if (cursors.up.isDown && this.player.body.touching.down)
         {
-
-          
             this.player.setVelocityY(-330);
         }
     }
